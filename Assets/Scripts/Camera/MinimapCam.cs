2f9bf77e08da71aae7acce3564348de8424a553c
John Dennis Thottam <johndthottam@gmail.com>
2017-09-27 04:25:49 +0530

    Minimap Implemented !
    A minimap to help players spot enemies . Enemies are colour coded as Red and the player as Green.

---
+++ Assets/Scripts/Camera/MinimapCam.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public class MinimapCam : MonoBehaviour
+{
+	public Transform Player;
+	public Vector3 Offset;
+
+	void LateUpdate ()
+	{
+		if (Player != null)
+			transform.position = Player.position + Offset;
+	}
+}
